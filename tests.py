619a2c6ee4cf51559e332482473599a96ffdcec1
Ariadeath <konradsgraeff@gmail.com>
2025-07-24 13:57:52 -0700

    Added feedback loop

--- tests.py
+++ tests.py
@@ -1,21 +1,19 @@
-from functions.get_files_info import get_files_info, get_file_content, write_file, run_python_file
+from functions.run_python import run_python_file
 
-calc_main_results = run_python_file("calculator", "main.py")
-print("Results for main.py contents")
-print(f"{calc_main_results}")
 
-calc_math_results = run_python_file("calculator", "main.py", ["3 + 5"])
-print("Results for math using main.py")
-print(f"{calc_math_results}")
+def test():
+    result = run_python_file("calculator", "main.py")
+    print(result)
 
-calc_tests_result = run_python_file("calculator", "tests.py")
-print("Results for tests.py contents")
-print(f"{calc_tests_result}")
+    result = run_python_file("calculator", "tests.py")
+    print(result)
 
-calc_first_error = run_python_file("calculator", "../main.py")
-print("Results for first error contents")
-print(f"{calc_first_error}")
+    result = run_python_file("calculator", "../main.py")
+    print(result)
 
-calc_second_error = run_python_file("calculator", "nonexistent.py")
-print("Results for second error")
-print(f"{calc_second_error}")
+    result = run_python_file("calculator", "nonexistent.py")
+    print(result)
+
+
+if __name__ == "__main__":
+    test()
