f48cde02615593603633f8b0bfa58ab6cd0d6c9c
Ariadeath <konradsgraeff@gmail.com>
2025-07-24 10:41:53 -0700

    Added ability to run python programs and tests

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 from .config import MAX_CHARS
 
 def get_files_info(working_directory, directory=None):
@@ -70,3 +71,37 @@ def write_file(working_directory, file_path, content):
     
     except Exception as e:
         return f"Error: {e}"
+    
+def run_python_file(working_directory, file_path, args=[]):
+    try:
+        joined_path = os.path.join(working_directory, file_path)
+        absolute_joined = os.path.abspath(joined_path)
+        absolute_working = os.path.abspath(working_directory)
+        
+        if not absolute_joined.startswith(absolute_working):
+            return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
+        
+        if not os.path.exists(absolute_joined):
+            return f'Error: File "{file_path}" not found.'
+        
+        if not absolute_joined.endswith(".py"):
+            return f'Error: "{file_path}" is not a Python file.'
+        
+        python_command = ["python", f"{absolute_joined}",]
+        python_command.extend(args)
+        completed_process = subprocess.run(python_command, timeout=30, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd = absolute_working)
+        decoded_stdout = completed_process.stdout.decode('utf-8')
+        decoded_stderr = completed_process.stderr.decode('utf-8')
+        output_string = ""
+        if decoded_stdout != "":
+            output_string += f"STDOUT: {decoded_stdout}\n"
+        if decoded_stderr != "":
+            output_string += f"STDERR: {decoded_stderr}\n"
+        if completed_process.returncode != 0:
+            output_string += f"Process exited with code {completed_process.returncode}"
+        if output_string == "":
+            return "No output produced"
+        return output_string
+    
+    except Exception as e:
+        return f"Error: executing Python file: {e}"
--- tests.py
+++ tests.py
@@ -1,13 +1,21 @@
-from functions.get_files_info import get_files_info, get_file_content, write_file
+from functions.get_files_info import get_files_info, get_file_content, write_file, run_python_file
 
-calc_main_results = write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
+calc_main_results = run_python_file("calculator", "main.py")
 print("Results for main.py contents")
 print(f"{calc_main_results}")
 
-calc_pkg_results = write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
-print("Results for pkg/calculator.py contents")
-print(f"{calc_pkg_results}")
+calc_math_results = run_python_file("calculator", "main.py", ["3 + 5"])
+print("Results for math using main.py")
+print(f"{calc_math_results}")
 
-calc_bin_results = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
-print("Results for bin/cat contents")
-print(f"{calc_bin_results}")
+calc_tests_result = run_python_file("calculator", "tests.py")
+print("Results for tests.py contents")
+print(f"{calc_tests_result}")
+
+calc_first_error = run_python_file("calculator", "../main.py")
+print("Results for first error contents")
+print(f"{calc_first_error}")
+
+calc_second_error = run_python_file("calculator", "nonexistent.py")
+print("Results for second error")
+print(f"{calc_second_error}")
